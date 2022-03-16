02e45fd32936b87831f2e9b1779978052646911b
IngenieroJohn <89933392+IngenieroJohn@users.noreply.github.com>
2022-03-16 12:25:29 -0500

    Update Plan_Electrica.java

--- src/Plan_Electrica.java
+++ src/Plan_Electrica.java
@@ -1,26 +1,20 @@
 
 public class Plan_Electrica {
 	
-	private int id;
+	
 	private String nombre;
 	private double tarifa;
 	private Ciudad city;
 	
-	public Plan_Electrica(int id, String name, double tarifa, Ciudad city) {
-		this.id= id;
+	public Plan_Electrica( String name, Ciudad city) {
+		
 		this.nombre=  name;
-		this.tarifa= tarifa;
+		
 		this.city= city;
 		
 	}
 
-	public int getId() {
-		return id;
-	}
 
-	public void setId(int id) {
-		this.id = id;
-	}
 
 	public String getNombre() {
 		return nombre;
@@ -30,20 +24,14 @@ public class Plan_Electrica {
 		this.nombre = nombre;
 	}
 
-	public double getTarifa() {
-		return tarifa;
-	}
 
-	public void setTarifa(double tarifa) {
-		this.tarifa = tarifa;
-	}
 	
 	
 	
 	@Override
 	public String toString() {
 		// TODO Auto-generated method stub
-		return "ID: "+ id +" Planta Electrica: " +nombre+" Tarifa: " +tarifa  + city;
+		return " Planta Electrica: " +nombre  + city;
 	}
 	
 
